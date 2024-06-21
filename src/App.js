bbe9552b533c1715f9cfe45f6d3b4eba40ee7427
Jed Powell <jed@workbit.io>
2024-06-21 18:16:03 +0100

    feat: add todo item

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./css/App.css";
 import Header from "./js/Header";
 import Form from "./js/Form";
 import ToDo from "./js/ToDo";
-
-const todos = [
-  { id: 1, title: "item1", description: "item1 description" },
-  { id: 2, title: "item2", description: "item2 description" },
-  { id: 3, title: "item3", description: "item3 description" },
-];
+import { useState } from "react";
+import data from "./data.json";
 
 function App() {
-  const toDoItems = todos.map((todo) => {
+  const [toDos, setToDos] = useState(data); // Initiate using data from json file
+
+  const addToDo = (toDo) => {
+    setToDos([...toDos, toDo]); // makes a copy of toDos, add a new item then stores the new array. Must be in square brackets
+  };
+
+  // creates array of toDo components
+  const toDoItems = toDos.map((todo) => {
     return (
       <ToDo key={todo.id} title={todo.title}>
         {todo.description}
@@ -22,8 +25,8 @@ function App() {
     <div className="app">
       <Header />
       <div className="content">
-        <Form />
-        <div className="toDo__grid">{toDoItems}</div>
+        <Form addToDo={addToDo} />
+        <div className="toDo__grid">{toDoItems}</div> /*React processes array of components in a block.*/
       </div>
     </div>
   );
--- src/js/Form.js
+++ src/js/Form.js
@@ -1,18 +1,29 @@
 import "../css/Form.css"
 
-export default function Form() {
-  function onSubmit() {
-    console.log("submitted");
+let nextId = 4; // each item must have unique key value
+
+export default function Form({addToDo}) {
+  const handleSubmit = (e, addToDo) => {
+    e.preventDefault();
+    const toDo = {
+      id: nextId++,
+      title: e.target.title.value,
+      description: e.target.desc.value
+    }
+    
+    addToDo(toDo);
+    return;
   }
 
   return (
     <div className="todo__form">
-      <form>
+      <h3>Create To-Do</h3>
+      <form onSubmit={(e) => handleSubmit(e, addToDo)}>
         <label htmlFor="title">Title</label>
         <input type="text" name="title" />
         <label htmlFor="desc">Description</label>
         <textarea name="desc"></textarea>
-        <button onSubmit={onSubmit}>Create</button>
+        <button>Create</button>
       </form>
     </div>
   );
