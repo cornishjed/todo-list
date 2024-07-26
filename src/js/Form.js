0d72b4f5babf69ed2226ba66199a2b54f61b7067
Jed Powell <jed@workbit.io>
2024-07-26 10:10:12 +0100

    fix: form rework

--- src/js/Form.js
+++ src/js/Form.js
@@ -1,32 +1,40 @@
+import { useState } from "react";
+
 import "../css/Form.css"
 
 let nextId = 4; // each item must have unique key value
 
 export default function Form({toDos, setToDos}) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  }
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const handleSubmit = () => {    
     const toDo = {
       id: nextId++,
-      title: e.target.title.value,
-      description: e.target.desc.value
+      title: title,
+      description: description
     }
     
     setToDos([...toDos, toDo]);
-
-    return;
   }
 
   return (
     <div className="todo__form">
       <h3>Create To-Do</h3>
-      <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="title">Title</label>
-        <input type="text" name="title" />
+        <input type="text" value={title} onChange={handleTitleChange} />
         <label htmlFor="desc">Description</label>
-        <textarea name="desc"></textarea>
-        <button>Create</button>
-      </form>
+        <textarea value={description} onChange={handleDescriptionChange} />
+        <button onClick={handleSubmit}>Create</button>
     </div>
   );
 }
