79c4f07493ca9b9edb3e1cbb794e0932b79a32b4
Jed Powell <jed@workbit.io>
2024-07-26 10:26:54 +0100

    fix: todo delete rework

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -5,11 +5,7 @@ import "../css/ToDo.css";
 export default function ToDo({ id, toDos, setToDos, title, children }) {
   const [isComplete, setComplete] = useState(false);
 
-  const handleDelete = (e) => {
-    e.preventDefault();
-
-    const id = e.target.id.value;
-
+  const handleDelete = (id) => {
     // find toDo item in array and delete
     const toDosCpy = [...toDos];
 
@@ -26,13 +22,12 @@ export default function ToDo({ id, toDos, setToDos, title, children }) {
 
   return (
     <div className={isComplete ? "toDo__card complete" : "toDo__card"}>
-      <form onSubmit={(e) => handleDelete(e)}>
-        <input type="hidden" name="id" value={id} />
-        <h1>{title}</h1>
-        <p>{children}</p>
-        <button>Delete</button>
-      </form>
-      <button onClick={isComplete ? () => setComplete(false) : () => setComplete(true)}>Mark {isComplete ? "Incomplete" : "Complete"}</button>
+      <h1>{title}</h1>
+      <p>{children}</p>
+      <div className="toDo__card-buttons">
+        <button onClick={() => handleDelete(id)}>Delete</button>
+        <button onClick={isComplete ? () => setComplete(false) : () => setComplete(true)}>Mark {isComplete ? "Incomplete" : "Complete"}</button>
+      </div>
     </div>
   );
 }
